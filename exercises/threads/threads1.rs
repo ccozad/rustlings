ff012602d271dd6b148f51e65a56b7dccf275e92
ccozad <ccozad@gmail.com>
2022-01-27 22:58:33 -0800

    Progress to date

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -19,14 +19,17 @@ struct JobStatus {
 fn main() {
     let status = Arc::new(JobStatus { jobs_completed: 0 });
     let status_shared = status.clone();
-    thread::spawn(move || {
+    let handle = thread::spawn(move || {
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
+            println!("hi from the spawned thread!");
             status_shared.jobs_completed += 1;
         }
     });
-    while status.jobs_completed < 10 {
-        println!("waiting... ");
+
+    handle.join().unwrap();
+    /*while status.jobs_completed < 10 {
+        println!("Main thread waiting... ");
         thread::sleep(Duration::from_millis(500));
-    }
+    }*/
 }
